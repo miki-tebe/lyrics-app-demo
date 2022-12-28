75186a68b106ada9f40c82867a9a1f68bb0c7040
Mikiyas Tibebu <mikitebe123@gmail.com>
2022-12-29 01:01:13 +0300

    added artist router and page

---
+++ src/server/trpc/router/artist.ts
@@ -0,0 +1,25 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const artistRouter = router({
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ input, ctx }) => {
+      const { id } = input;
+      const artist = ctx.prisma.artist.findUnique({
+        where: { id },
+      });
+      if (!artist) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: `No post with id '${id}'`,
+        });
+      }
+      return artist;
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.artist.findMany();
+  }),
+});
